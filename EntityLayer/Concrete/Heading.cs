ab0c8ee4c219fecbe1f5857f280524b1d261b65a
Cafer Gökmen Portakal <cgportakal@outlook.com>
2021-04-27 18:49:54 +0300

    Katmanlar eklendi. Entity Katmanı ilişkileri oluşturuldu Entity Framework Kuruldu. Primary Key ve Sütun kısıtlamaları eklendi.Nuget paketleri güncellendi ve hatalar giderildi.

---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        [Key]
+        public int HeadingId { get; set; }
+        [StringLength(50)]
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+
+        
+        //Category tablosu ile ilişki kurduk.
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        //Başlık ile İçeriği bağlantısın kurduk.
+        //1 başlık çok içerik.
+        public ICollection<Content> Contents { get; set; }
+
+        //Yazar ile ilişki kurduk.
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        [Key]
+        public int WriterId { get; set; }
+        [StringLength(50)]
+        public string WriterName { get; set; }
+        [StringLength(50)]
+        public string WriterSurname { get; set; }
+        [StringLength(100)]
+        public string WriterImage { get; set; }
+        [StringLength(70)]
+        public string WriterMail { get; set; }
+        [StringLength(25)]
+        public string WriterPassword { get; set; }
+
+        //Heading ile ilişki kurduk.
+        public ICollection<Heading> Headings { get; set; }
+        //Content(içerik) ile ilişki kurduk.
+        public ICollection<Content> Contents { get; set; }
+    }
+}
